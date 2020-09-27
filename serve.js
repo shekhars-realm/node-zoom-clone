0f17554b1924b33d6e8ad5a1637f38668482c930
Sam <sam@Sams-MacBook-Air.local>
2020-09-27 23:44:20 +0200

    gitignore added and ejs created

--- serve.js
+++ serve.js
@@ -4,8 +4,21 @@ const app = express();
 
 const server = require('http').Server(app);
 
+const {v4: uuidv4} = require('uuid')
+
+app.set('view engine', 'ejs')
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('HEllo world')
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', {
+        roomId: req.params.room
+    })
 })
 
 
